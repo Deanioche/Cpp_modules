dfa450de39f79fbda3a43f94dbf2eeeed7dfd31c
Soksurim <briochefavori@gmail.com>
2023-01-02 15:08:34 +0900

    fixed main test cases in ex01

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -1,36 +1,66 @@
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 
-int	main(void)
+int	f(void)
 {
-	ClapTrap claptrap("A");
-	ScavTrap scavtrap("B");
+	ClapTrap clapTrap("Parent");
+	ScavTrap scavTrap("Kid");
 	std::cout << std::endl;
 
-	claptrap.attack("B");
-	scavtrap.takeDamage(0);
-	scavtrap.beRepaired(18);
+	clapTrap.attack("Kid");
+	scavTrap.takeDamage(0);
+	scavTrap.attack("Parent");
+	scavTrap.takeDamage(20);
 	std::cout << std::endl;
 
-	scavtrap.attack("A");
-	claptrap.takeDamage(20);
-	scavtrap.attack("A");
-	claptrap.takeDamage(20);
-	scavtrap.attack("A");
-	claptrap.beRepaired(64);
+	scavTrap.guardGate();
 	std::cout << std::endl;
 
-	scavtrap.guardGate();
-	scavtrap.attack("A");
+	scavTrap.beRepaired(1000);
+	scavTrap.takeDamage(1000);
+	scavTrap.attack("Parent");
+	clapTrap.takeDamage(20);
 	std::cout << std::endl;
 
-	scavtrap.takeDamage(100);
-	scavtrap.beRepaired(200);
-	scavtrap.attack("B");
-	scavtrap.takeDamage(300);
-	scavtrap.takeDamage(1);
-	scavtrap.beRepaired(1);
+	clapTrap.attack("Kid");
+	clapTrap.beRepaired(100);
+	clapTrap.takeDamage(1);
 	std::cout << std::endl;
+
+	scavTrap.takeDamage(1000);
+	scavTrap.attack("Parent");
+	scavTrap.takeDamage(20);
+	scavTrap.beRepaired(20);
+	scavTrap.guardGate();
+	std::cout << std::endl;
+
+	// scavTrap.attack("Parent");
+	// clapTrap.takeDamage(20);
+	// scavTrap.attack("Parent");
+	// clapTrap.takeDamage(20);
+	// scavTrap.attack("Parent");
+	// clapTrap.beRepaired(64);
+	// std::cout << std::endl;
+
+	// scavTrap.attack("Parent");
+	// std::cout << std::endl;
+
+	// scavTrap.takeDamage(100);
+	// scavTrap.beRepaired(200);
+	// scavTrap.attack("Kid");
+	// scavTrap.takeDamage(300);
+	// scavTrap.takeDamage(1);
+	// scavTrap.beRepaired(1);
+	// std::cout << std::endl;
 
 	return (0);
+}
+
+#include <string>
+#include <unistd.h>
+int main(void)
+{
+	f();
+	system(("leaks " + std::to_string(getpid())).c_str());
+	return 0;
 }
